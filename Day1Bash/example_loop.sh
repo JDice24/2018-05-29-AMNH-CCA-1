caa1de1e37c392518e97b122c114da958234c3f4
Camille Avestruz <camille.avestruz@yale.edu>
2018-05-29 14:24:58 -0500

    Removed excess lines from example_loop.sh

--- Day1Bash/example_loop.sh
+++ Day1Bash/example_loop.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-# I am creating a variable greeting
-greeting='Hello World'
-
-# I am printing the contents of greeting
-echo $greeting
-
-# Touch and print a range of files in a loop
-
 for myfile in {A..Z}.txt;
 do
     touch $myfile
