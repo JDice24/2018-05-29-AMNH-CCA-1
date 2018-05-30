970c7660f54087e99c376b2437d76ab2830f9d1e
Camille Avestruz <camille.avestruz@yale.edu>
2018-05-30 14:23:35 -0500

    added an example module, functions_to_use, from which we can import methods and attributes.

---
+++ Day2Python/functions_to_use.py
@@ -0,0 +1,10 @@
+'''This module contains some functions we will create.'''
+
+def division_function_to_test(dividend, divisor) :
+    ''' Always returns the float value of dividend/divisor '''
+
+    return dividend * 1.0/divisor
+
+
+# Example attribute
+example_dividend = 300
