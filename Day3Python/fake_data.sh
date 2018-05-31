fe44df18501a35439dae109bd8dd2a53dde7f432
Camille Avestruz <camille.avestruz@yale.edu>
2018-05-31 15:30:42 -0500

    added a script to generate fake data

---
+++ Day3Python/fake_data.sh
@@ -0,0 +1,12 @@
+# This script will generate a two column data file comprise of 1 through 10 in one column, and twice that in another column
+
+# Let's create a header
+echo 'xaxis yaxis' > data/linear_data.dat
+
+# Let's perform a for loop in order to create the columns
+for xdat in {1..10};
+do
+    echo $xdat $((xdat*2)) >> data/linear_data.dat
+done
+
+echo blah blah
